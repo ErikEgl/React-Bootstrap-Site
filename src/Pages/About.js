881889ad046b32b52b8b750ea03dfcc4c03b24e5
Erik Egliens <erik@bellcom.ee>
2022-01-08 01:19:00 +0200

    added tabs and contact form

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,11 +1,70 @@
 import React, { Component } from 'react';
+import { Col, Container, Nav, Tab, Row } from 'react-bootstrap';
 
 export default class About extends Component {
     render() {
         return (
-            <div>
-                Hello About
-            </div>
+           <Container>
+               <Tab.Container id="ledt-tabs-example" defaultActiveKey="first" >
+                    <Row>
+                        <Col sm={3}>
+                            <Nav variant='pills' className='flex-column mt-2'>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">
+                                        Design
+                                    </Nav.Link>
+                                    <Nav.Link eventKey="second">
+                                        Team
+                                    </Nav.Link>
+                                    <Nav.Link eventKey="third">
+                                        Programming
+                                    </Nav.Link>
+                                    <Nav.Link eventKey="fourth">
+                                        Frameworks
+                                    </Nav.Link>
+                                    <Nav.Link eventKey="fifth">
+                                        Libraries
+                                    </Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Col>
+                        <Col sm={9}>
+                            <Tab.Content className='mt-3'>
+                                <Tab.Pane eventKey="first">
+                                    <img src="https://media.istockphoto.com/photos/winter-hiking-picture-id531106307?k=20&m=531106307&s=612x612&w=0&h=IZRjS844stJeqDn1bBja4hsv2RTLPzAsSHxZqZluGZk="/>
+                                    <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa
+                                    </p>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">
+                                    <img src="https://media.istockphoto.com/photos/ice-crystal-picture-id925624048?k=20&m=925624048&s=612x612&w=0&h=w3p_0xzsAONPPUWPxydZ3VlVT_VDOBS7QsNrNEj8zGE="/>
+                                    <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa
+                                    </p>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="third">
+                                    <img src="https://media.istockphoto.com/photos/tilted-blue-version-ice-skates-with-reflection-picture-id511302560?k=20&m=511302560&s=612x612&w=0&h=eswxcZflBUEwgq-lAgERgkNUd8QGamN9NV5M7rHo5TA="/>
+                                    <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa
+                                    </p>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="fourth">
+                                    <img src="https://media.istockphoto.com/photos/walking-the-mountains-picture-id649597850?k=20&m=649597850&s=612x612&w=0&h=EkjZ5iwG0rsV3RuYfEhHtQDLlE44NTv5m4ZhczLZHPY="/>
+                                    <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa
+                                    </p>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="fifth">
+                                    <img src="https://media.istockphoto.com/photos/female-hiker-standing-in-the-snow-picture-id1220996921?k=20&m=1220996921&s=612x612&w=0&h=ftRnmOdG6b1ZJf7vtqm_ThWy5ZfNeTfbSCnDnqxDOss="/>
+                                    <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa
+                                    </p>
+                                </Tab.Pane>
+                            </Tab.Content>
+                        </Col>
+                    </Row>
+               </Tab.Container>
+           </Container>
         );
     }
 }
--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Container, Form, Button } from "react-bootstrap";
 
 export default class Contacts extends Component {
-    render() {
-        return (
-            <div>
-                Hello Contacts
-            </div>
-        );
-    }
+  render() {
+    return (
+      <Container style={{width: '500px'}}>
+        <h1 className='text-center'>Contact us</h1>
+        <Form>
+          <Form.Group controlId='formBasicEmail'>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control type='email' placeholder='Enter Email'/>
+            <Form.Text>We will not share your email with anyone else</Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId='formBasicPassword'>
+            <Form.Label>Textarea</Form.Label>
+            <Form.Control rows='3' as='textarea'/>
+          </Form.Group>
+
+          <Form.Group controlId='formBasicCheckbox'>
+            <Form.Label>Textarea</Form.Label>
+            <Form.Check type='checkbox' label='Check me out'/>
+          </Form.Group>
+            <Button variant='primary' type='submit'>Submit</Button>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 // export default Contacts;
