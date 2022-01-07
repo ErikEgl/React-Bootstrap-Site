99073386c547cac981e710cebe0226eca99d082e
Erik Egliens <erik@bellcom.ee>
2022-01-08 01:39:28 +0200

    cards

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,57 @@
-import React, { Component } from 'react';
-import CarouselBox from '../Components/CarouselBox';
-import Carousel from 'react-bootstrap/Carousel';
+import React, { Component } from "react";
+import CarouselBox from "../Components/CarouselBox";
+import { Container, Button, Card, Row } from "react-bootstrap";
 
 
 export default class Home extends Component {
-    render() {
-        return (
-            <CarouselBox />
-        );
-    }
+  render() {
+    return (
+      <>
+        <CarouselBox />
+        <Container>
+          <h1 className="text-center m-4">Our team</h1>
+          <Row  md={4}>
+              <Card className='m-1'>
+                  <Card.Img variant='top' src='https://thelearningrooms.com/wps/wp-content/uploads/2015/02/team.jpg'/>
+                    <Card.Body>
+                        <Card.Title> Developers</Card.Title>
+                        <Card.Text>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa
+                        </Card.Text>
+                        <Button variant="primary">
+                            About Team
+                        </Button>
+                    </Card.Body>
+              </Card>
+              <Card className='m-1'>
+                  <Card.Img variant='top' src='https://thelearningrooms.com/wps/wp-content/uploads/2015/02/team.jpg'/>
+                    <Card.Body>
+                        <Card.Title> Developers</Card.Title>
+                        <Card.Text>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa
+                        </Card.Text>
+                        <Button variant="primary">
+                            About Team
+                        </Button>
+                    </Card.Body>
+              </Card>
+              <Card className='m-1'>
+                  <Card.Img variant='top' src='https://thelearningrooms.com/wps/wp-content/uploads/2015/02/team.jpg'/>
+                    <Card.Body>
+                        <Card.Title> Developers</Card.Title>
+                        <Card.Text>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa
+                        </Card.Text>
+                        <Button variant="primary">
+                            About Team
+                        </Button>
+                    </Card.Body>
+              </Card>
+          </Row>
+        </Container>
+      </>
+    );
+  }
 }
 
-    // export default Home;
+// export default Home;
