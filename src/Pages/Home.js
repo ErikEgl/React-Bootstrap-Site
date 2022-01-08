bf6e26f90236ae02dcd70dd854806e73261b37fa
Erik Egliens <erik@bellcom.ee>
2022-01-08 02:03:47 +0200

    added blog and htaccess

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import CarouselBox from "../Components/CarouselBox";
-import { Container, Button, Card, Row } from "react-bootstrap";
-
+import { Container, Button, Card, Row, Col } from "react-bootstrap";
 
 export default class Home extends Component {
   render() {
@@ -10,43 +9,55 @@ export default class Home extends Component {
         <CarouselBox />
         <Container>
           <h1 className="text-center m-4">Our team</h1>
-          <Row  md={4}>
-              <Card className='m-1'>
-                  <Card.Img variant='top' src='https://thelearningrooms.com/wps/wp-content/uploads/2015/02/team.jpg'/>
-                    <Card.Body>
-                        <Card.Title> Developers</Card.Title>
-                        <Card.Text>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa
-                        </Card.Text>
-                        <Button variant="primary">
-                            About Team
-                        </Button>
-                    </Card.Body>
+          <Row>
+            <Col>
+              <Card className="m-1">
+                <Card.Img
+                  variant="top"
+                  src="https://thelearningrooms.com/wps/wp-content/uploads/2015/02/team.jpg"
+                />
+                <Card.Body>
+                  <Card.Title> Developers</Card.Title>
+                  <Card.Text>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Eligendi non quis exercitationem culpa
+                  </Card.Text>
+                  <Button variant="primary">About Team</Button>
+                </Card.Body>
               </Card>
-              <Card className='m-1'>
-                  <Card.Img variant='top' src='https://thelearningrooms.com/wps/wp-content/uploads/2015/02/team.jpg'/>
-                    <Card.Body>
-                        <Card.Title> Developers</Card.Title>
-                        <Card.Text>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa
-                        </Card.Text>
-                        <Button variant="primary">
-                            About Team
-                        </Button>
-                    </Card.Body>
+              </Col>
+              <Col>
+              <Card className="m-1">
+                <Card.Img
+                  variant="top"
+                  src="https://thelearningrooms.com/wps/wp-content/uploads/2015/02/team.jpg"
+                />
+                <Card.Body>
+                  <Card.Title> Developers</Card.Title>
+                  <Card.Text>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Eligendi non quis exercitationem culpa
+                  </Card.Text>
+                  <Button variant="primary">About Team</Button>
+                </Card.Body>
               </Card>
-              <Card className='m-1'>
-                  <Card.Img variant='top' src='https://thelearningrooms.com/wps/wp-content/uploads/2015/02/team.jpg'/>
-                    <Card.Body>
-                        <Card.Title> Developers</Card.Title>
-                        <Card.Text>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa
-                        </Card.Text>
-                        <Button variant="primary">
-                            About Team
-                        </Button>
-                    </Card.Body>
+              </Col>
+              <Col>
+              <Card className="m-1">
+                <Card.Img
+                  variant="top"
+                  src="https://thelearningrooms.com/wps/wp-content/uploads/2015/02/team.jpg"
+                />
+                <Card.Body>
+                  <Card.Title> Developers</Card.Title>
+                  <Card.Text>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Eligendi non quis exercitationem culpa
+                  </Card.Text>
+                  <Button variant="primary">About Team</Button>
+                </Card.Body>
               </Card>
+            </Col>
           </Row>
         </Container>
       </>
